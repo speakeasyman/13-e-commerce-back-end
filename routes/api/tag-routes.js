72bef5e58618b184a00e81d7c9a8da554c095a01
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-29 15:30:09 -0400

    tag-routes completed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,71 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = require("express").Router();
+const { Tag, Product, ProductTag } = require("../../models");
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+
+  Tag.findAll({
+    include: [{ model: Product, through: ProductTag }],
+  })
+    .then((tagAll) => {
+      res.json(tagAll);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    include: [{ model: Product, through: ProductTag }],
+  })
+    .then((tagId) => {
+      res.json(tagId);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+    .then((tagNew) => {
+      res.json(tagNew);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.put('/:id', (req, res) => {
+router.put("/:id", (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      whree: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((tagUpdate) => {
+      res.json(tagUpdate);
+    })
+    .catch((err) => res.json(err));
 });
 
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((tagDelete) => {
+      res.json(tagDelete);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
