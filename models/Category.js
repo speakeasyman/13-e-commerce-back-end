00ae77ae9c9b77f0d385124e36477d9c02d349a5
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-06-28 23:08:28 -0400

    ProductTag.js is completed, also prettified other .js files

--- models/Category.js
+++ models/Category.js
@@ -1,6 +1,6 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
@@ -16,14 +16,14 @@ Category.init(
     category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
